3edfabf7d62bbc88e6bc06990ded2e5fb2247a1e
rtof <zuichuan@msn.com>
2022-07-16 18:35:46 -0300

    aws

---
+++ aws/README.md
@@ -0,0 +1,15 @@
+## API
+- `https://5ngovpktp9.execute-api.us-east-1.amazonaws.com`
+
+### ROUTES (GET, PUT)
+- `https://5ngovpktp9.execute-api.us-east-1.amazonaws.com/v1`
+
+### ROUTES WITH ID (GET, POST, DELETE)
+- `https://5ngovpktp9.execute-api.us-east-1.amazonaws.com/v1/{id}`
+
+
+## DynamoDB struture
+- `{`
+    `id: string,`
+    `name: string`
+  `}`
---
+++ aws/lambda.js
@@ -0,0 +1,87 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const TableName = "store";
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = { "Content-Type": "application/json" };
+
+  try {
+    switch (event.routeKey) {
+      
+      case "DELETE /v1/{id}":
+        await dynamo
+          .delete({
+            TableName: TableName,
+            Key: {
+              id: event.pathParameters.id
+            },
+          })
+          .promise();
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+        
+      case "GET /v1":
+        body = await dynamo.scan({ TableName: TableName }).promise();
+        break;
+        
+      case "GET /v1/{id}":
+        body = await dynamo
+          .get({
+            TableName: TableName,
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        break;
+        
+      case "PUT /v1":
+        console.log("Processing...");
+        let requestJSON = JSON.parse(event.body);
+        await dynamo
+          .put({
+            TableName: TableName,
+            Item: {
+              id: requestJSON.id,
+              name: requestJSON.name
+            }
+          })
+          .promise();
+        body = `Item ${requestJSON.message} added`;
+        break;
+        
+      case "POST /v1/{id}":
+        await dynamo
+          .update({
+            TableName: TableName,
+            Key: { id: event.pathParameters.id },
+            UpdateExpression: "set #n = :name",
+            ExpressionAttributeValues: {
+              ":name": JSON.parse(event.body).name
+            },
+            ExpressionAttributeNames: {
+              "#n": "name"
+            },
+            ReturnValues: "UPDATED_NEW"
+          }).promise();
+        body = `Item ${event.pathParameters.id} updated`;
+        break;
+        
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+};
